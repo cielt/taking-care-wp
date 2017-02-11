a345b04a129c1746924a9aa81d55b63f71b6e92a
leslie tu <chiayin.l.tu@gmail.com>
2017-02-11 13:48:35 -0500

    fix path to favicon

--- header.php
+++ header.php
@@ -16,7 +16,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="icon" href="<?php echo get_template_directory() ?>/images/favicon.ico" type="image/x-icon" />
+<link rel="icon" href="<?php echo get_template_directory_uri() ?>/images/favicon.ico" type="image/x-icon" />
 
 <?php wp_head(); ?>
 </head>
